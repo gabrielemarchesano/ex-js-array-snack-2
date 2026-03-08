51466c40c4ed3c8acd40cc66889959492b882872
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-03-08 16:28:31 +0100

    create ages array, sum the ages, find the average

---
+++ snack4/snack4.js
@@ -0,0 +1,65 @@
+const books = [
+  {
+    title: "React Billionaire",
+    pages: 250,
+    author: {
+      name: 'Alice',
+      age: 35
+    },
+    available: false,
+    price: '101€',
+    tags: ['advanced', 'js', 'react', 'senior']
+  },
+  {
+    title: "Advanced JS",
+    pages: 500,
+    author: {
+      name: 'Bob',
+      age: 20
+    },
+    available: true,
+    price: '25€',
+    tags: ['advanced', 'js', 'mid-senior']
+  },
+  {
+    title: "CSS Secrets",
+    pages: 320,
+    author: {
+      name: 'Alice',
+      age: 17
+    },
+    available: true,
+    price: '8€',
+    tags: ['html', 'css', 'junior']
+  },
+  {
+    title: "HTML Mastery",
+    pages: 200,
+    author: {
+      name: 'Charlie',
+      age: 50
+    },
+    available: false,
+    price: '48€',
+    tags: ['html', 'advanced', 'junior', 'mid-senior']
+  },
+];
+
+/* Snack 4 - Calcola l'età media
+Creare un array (ages) che contiene le età degli autori dei libri.
+Calcola la somma delle età (agesSum) usando reduce.
+Stampa in console l’età media degli autori dei libri.
+*/
+
+const authors = books.map(book => book.author);
+
+const ages = authors.map(author => author.age);
+console.log(ages);
+
+const agesSum = ages.reduce((accumulatore, age) => {
+  return accumulatore + age;
+}, 0);
+console.log(agesSum);
+
+const average = agesSum / ages.length;
+console.log(average)
